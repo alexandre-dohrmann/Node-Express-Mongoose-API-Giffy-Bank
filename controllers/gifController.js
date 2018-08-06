0a9d15807c81f57b65a69544b7e6c6f487426a7d
James M <saintjames@MacBook-Pro-2.local>
2018-08-06 14:42:21 -0600

    add basic layout to express

---
+++ controllers/gifController.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+const GIF = require ('../models/gif');
+
+
+// Index Route
+router.get('/', async (req, res, next) => {
+console.log("Index Route");
+    try{
+        const allGIF = await GIF.find();
+
+        res.json({
+            status: 200,
+            data: allGIF
+        });
+
+    } catch (err) {
+        res.send(err)
+        console.log(err);
+    }
+});
+
+
+// Create Route
+router.post('/', async (req, res) => {
+console.log("Create Route");
+    try {
+        const createdGIF = await GIF.create(req.body);
+
+        req.session.logged = true;
+        req.session.username = req.body.username;
+
+        res.json({
+            status: 200,
+            data: createdMovie
+        });
+
+    } catch(err) {
+        res.send(err)
+        console.log(err);
+    }
+});
+
+
+// Show Route
+router.get('/', async (req, res, next) => {
+console.log("Show Route");
+    try {
+        const foundGIF = await GIF.findById(req.params.id);
+
+        res.json({
+            status: 200,
+            data: foundGIF
+        });
+
+    } catch(err) {
+        res.send(err)
+        console.log(err);
+    }
+});
+
+
+// Update Route
+router.put('/:id', async (req, res) => {
+console.log("Update Route");
+    try {
+        const updatedGIF = await GIF.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        res.json({
+            status: 200,
+            data: updatedGIF
+        });
+
+    } catch(err) {
+        res.send(err);
+    }
+});
+
+
+// Delete Route
+router.delete('/:id', async (req, res) => {
+
+    try {
+        const deletedGIF = await GIF.findByIdAndRemove(req.param.id);
+
+        res.json({
+            status: 200,
+            data: deletedGIF
+        });
+    } catch(err) {
+        res.send(err);
+    }
+});
+
+
+
+
+
+
+module.exports = router;
